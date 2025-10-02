60198ed961a9bd7fa075749bc0b09ab05f10b54c
Vishvajit Havale <vishvajithavale13@gmai.com>
2025-10-02 23:11:36 +0530

    added docker file

--- Excel compair/Program.cs
+++ Excel compair/Program.cs
@@ -7,14 +7,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ExcelCompair API V1");
+    c.RoutePrefix = string.Empty; // Swagger at root (https://yourapp.onrender.com)
+});
 
 app.UseHttpsRedirection();
 
